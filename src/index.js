8096fc1b17159cbb16d88c8bb40c74e25389fa15
ArtemKarlov <ar.karlov@gmail.com>
2021-03-09 11:07:27 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  const arr = [...array];
+  return arr.sort((a,b)=> (a-b))[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  const arr = [...array];
+  return arr.sort((a,b)=> (b-a))[0]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+  const sum = array.reduce((sum, current) => sum + current, 0);
+  return sum / array.length;
 }
